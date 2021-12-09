c85c9a1d415233efaa8c2a5a5a53cab00b41cd90
kimberley <kim.dobney@yahoo.co.uk>
2021-12-09 16:02:15 +0000

    added modals

--- modal.js
+++ modal.js
@@ -2,12 +2,60 @@ const modalButtonMuppets = document.getElementById('modalButtonMuppets')
 const popup = document.getElementById('modalPopup')
 const closeModal = document.getElementById('closeModal')
 
+const modalButtonPWWorld = document.getElementById('modalButtonPWWorld')
+const popupPWWorld = document.getElementById('modalPopupPWWorld')
+const closeModalPWWorld = document.getElementById('closeModalPWWorld')
+
+const modalButtonPlanet = document.getElementById('modalButtonPlanet')
+const popupPlanets = document.getElementById('modalPopupPlanets')
+const closeModalPlanets = document.getElementById('closeModalPlanets')
+
+const modalButtonSnakeSweeper = document.getElementById('modalButtonSnakeSweeper')
+const popupSnakeSweeper = document.getElementById('modalPopupSnakeSweeper')
+const closeModalSnakeSweeper = document.getElementById('closeModalSnakeSweeper')
+
 modalButtonMuppets.addEventListener('click', function (){
     popup.style.display = "flex";
     modalButtonMuppets.style.display = "none";
     closeModal.addEventListener('click',function (){
         popup.style.display = "none";
         modalButtonMuppets.style.display = "";
+        document.getElementById('page').style.filter = "none";
+    })
+    document.getElementById('page').style.filter = "blur(2px)";
+})
+
+modalButtonPWWorld.addEventListener('click', function (){
+    console.log("clicked")
+    popupPWWorld.style.display = "flex";
+    modalButtonPWWorld.style.display = "none";
+    closeModalPWWorld.addEventListener('click',function (){
+        popupPWWorld.style.display = "none";
+        modalButtonPWWorld.style.display = "";
+        document.getElementById('page').style.filter = "none";
+    })
+    document.getElementById('page').style.filter = "blur(2px)";
+})
+
+modalButtonPlanet.addEventListener('click', function (){
+    console.log("clicked")
+    popupPlanets.style.display = "flex";
+    modalButtonPlanet.style.display = "none";
+    closeModalPlanets.addEventListener('click',function (){
+        popupPlanets.style.display = "none";
+        modalButtonPlanet.style.display = "";
+        document.getElementById('page').style.filter = "none";
+    })
+    document.getElementById('page').style.filter = "blur(2px)";
+})
+
+modalButtonSnakeSweeper.addEventListener('click', function (){
+    console.log("clicked")
+    popupSnakeSweeper.style.display = "flex";
+    modalButtonSnakeSweeper.style.display = "none";
+    closeModalSnakeSweeper.addEventListener('click',function (){
+        popupSnakeSweeper.style.display = "none";
+        modalButtonSnakeSweeper.style.display = "";
         document.getElementById('page').style.filter = "none";
     })
     document.getElementById('page').style.filter = "blur(2px)";
